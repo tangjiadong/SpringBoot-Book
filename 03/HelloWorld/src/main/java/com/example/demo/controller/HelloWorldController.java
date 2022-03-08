8cfc99e21afb2c447f3a82da743fe0d5dff56bb0
tangjiadong <tang897396324@163.com>
2022-03-09 00:31:43 +0800

    tang-commit

--- 03/HelloWorld/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ 03/HelloWorld/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping(value="/hello")
     public String hello(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
         return "Hello,Spring Boot!";
     }
 
